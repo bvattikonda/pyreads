547602844d1e96525741e2a4483b66f5f6152a7f
Bhanu Vattikonda <vbhanu@gmail.com>
2013-06-05 11:35:10 -0700

    Starting to develop the API

---
+++ pyreads/__init__.py
@@ -0,0 +1,14 @@
+"""
+PyReads
+-------
+
+PyReads is a library for Python that wraps the GoodReads API.
+
+The aim of the package is to abstract out the GoodReads API and make the task of
+accessing the API simple.
+
+Questions, comments? bvattikonda@cs.ucsd.edu
+"""
+
+__author__ = 'Bhanu Vattikonda <bvattikonda@cs.ucsd.edu>'
+__version__ = '1.0.0'
