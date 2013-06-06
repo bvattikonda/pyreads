a7b8b5a762fd4ec00a4d4fa9148be8cba90950bb
Bhanu Vattikonda <vbhanu@gmail.com>
2013-06-06 10:40:37 -0700

    Adding a large number of moderately tested interfaces

--- pyreads/endpoints.py
+++ pyreads/endpoints.py
@@ -3,4 +3,64 @@ api_table = {
         'url': '/api/auth_user',
         'method': 'GET',
     },
+    'author_list': {
+        'url': '/author/list/{{id}}.xml',
+        'method': 'GET',
+    },
+    'author_show': {
+        'url': '/author/show/{{id}}.xml',
+        'method': 'GET',
+    },
+    'book_isbn_to_id': {
+        'url': '/book/isbn_to_id/{{id}}?key={{key}}',
+        'method': 'GET',
+    },
+    'book_review_counts': {
+        'url': '/book/review_counts.json?isbns={{isbns}}',
+        'method': 'GET',
+    },
+    'book_show': {
+        'url': '/book/show/{{id}}?format=xml',
+        'method': 'GET',
+    },
+    'book_isbn': {
+        'url': '/book/isbn?isbn={{isbn}}&format=xml&key={{key}}&user_id={{user_id}}&rating={{rating}}',
+        'method': 'GET',
+    },
+    'book_title': {
+        'url': '/book/title.xml?author={{author}}&key={{key}}&title={{title}}&format=xml&rating={{rating}}',
+        'method': 'GET',
+    },
+    'owned_books': {
+        'url': '/owned_books/user?format=xml&id={{id}}',
+        'method': 'GET',
+    },
+    'owned_books_show': {
+        'url': '/owned_books/show.xml?id={{id}}',
+        'method': 'GET',
+    },
+    'review_list': {
+        'url': '/review/list/{{id}}.xml?key={{key}}&v=2&shelf={{shelf}}&sort={{sort}}&search={{search}}&order={{order}}&page={{page}}&per_page={{per_page}}',
+        'method': 'GET',
+    },
+    'review_recent_reviews': {
+        'url': '/review/recent_reviews.xml?key={{key}}',
+        'method': 'GET',
+    },
+    'review_show_by_user_and_book': {
+        'url': '/review/show_by_user_and_book.xml?book_id={{book_id}}&key={{key}}&user_id={{user_id}}&include_review_on_work={{include_review_on_work}}',
+        'method': 'GET',
+    },
+    'author_url': {
+        'url': '/api/author_url/{{id}}?key={{key}}',
+        'method': 'GET',
+    },
+    'search': {
+        'url': '/search.xml?key={{key}}&q={{q}}',
+        'method': 'GET',
+    },
+    'shelf_list': {
+        'url': '/shelf/list.xml?key={{key}}&user_id={{user_id}}&page={{page}}',
+        'method': 'GET',
+    },
 }
--- pyreads/pyreads.py
+++ pyreads/pyreads.py
@@ -1,5 +1,9 @@
 import oauth2 as oauth
 from auth import OAuthHandler
+from endpoints import api_table
+import re
+import urllib
+import urlparse
 
 class PyReads(object):
     def __init__(self, app_key = None, app_secret = None,
@@ -13,14 +17,48 @@ class PyReads(object):
                                    proxy_info = proxies)
         self.headers = headers
 
-    def request(self, uri, method = 'GET'):
-        return self.client.request(self.api_url + uri, method, headers = self.headers)
+        def setFunction(key):
+            return lambda **kwargs: self._constructFunction(key, **kwargs)
+        
+        for key in api_table.keys():
+            self.__dict__[key] = setFunction(key)
+
+    def _update_query(self, query_string):
+        queries = [query.split('=', 1) if '=' in query else [query, ''] for query in query_string.split('&') if query]
+        params = {}
+        for key, value in queries:
+            if value == 'None':
+                continue
+            params[key] = value
+
+        return urllib.urlencode(params)
+
+    def _constructFunction(self, api_call, **kwargs):
+        fn = api_table[api_call]
+        url = re.sub(
+            '\{\{(?P<m>[a-zA-Z_]+)\}\}',
+            lambda m: "%s" % kwargs.get(m.group(1)),
+            self.api_url + fn['url']
+        )
+
+        parseResult = list(urlparse.urlparse(url))
+        parseResult[4] = self._update_query(parseResult[4])
+        url = urlparse.urlunparse(parseResult)
+        
+        content = self._request(url, method=fn['method'], params=kwargs)
+
+        return content
+
+    def _request(self, url, method = 'GET', params = None):
+        print url
+        body = urllib.urlencode(params)
+        return self.client.request(url, method, body = body, headers = self.headers)
 
 if __name__ == '__main__':
-    APP_KEY = 'jTvdwlSsuOOgtmQUw5JoXQ'
-    APP_SECRET = 'AwIKsQWPsYeWQFI2c76r2c2K0i7tKj8aqQEC60IqUQ'
-    OAUTH_TOKEN = 'RIAfo67nmhulCFS1rjebtA'
-    OAUTH_TOKEN_SECRET = 'SugrKz7NlUyifuWYRvd9zSGZzD4fpr5aD1ktTgvjBA'
+    APP_KEY = ''
+    APP_SECRET = ''
+    OAUTH_TOKEN = ''
+    OAUTH_TOKEN_SECRET = ''
 
     pyreads = PyReads(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-    print pyreads.request('/api/auth_user')
+    print pyreads.shelf_list(key = APP_KEY, user_id = '6462529')
