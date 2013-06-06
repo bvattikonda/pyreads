9e1b3d865732d8eb3cf52d5c3f3f5d29f7017b68
Bhanu Vattikonda <vbhanu@gmail.com>
2013-06-05 19:04:26 -0700

    Adding auth

--- pyreads/auth.py
+++ pyreads/auth.py
@@ -1,4 +1,3 @@
-from urllib2 import urlopen
 import oauth2 as oauth
 
 class AuthHandler(object):
@@ -14,11 +13,31 @@ class OAuthHandler(AuthHandler):
     def __init__(self, consumer_key, consumer_secret):
         self._consumer = oauth.Consumer(consumer_key, consumer_secret)
         self.request_token = None
+        self.access_token = None
         self.user = None
 
     def _get_oauth_url(self, endpoint):
         prefix = 'http://'
         return prefix + self.OAUTH_HOST + self.OAUTH_ROOT + endpoint
 
+    def set_access_token(self, key, secret):
+        self.access_token = oauth.Token(key, secret)
+
     def apply_auth(self, url, method, headers, parameters):
-        request = oau
+        request = oauth.Request.from_consumer_and_token(self._consumer, 
+                                                        http_url = url,
+                                                        http_method = method, 
+                                                        token = self.access_token, 
+                                                        parameters = parameters)
+        request.sign_request(self._sigmethod, self._consumer, self.access_token)
+        headers.update(request.to_header())
+
+if __name__ == '__main__':
+
+    """
+        This piece of code shows how to and helps generate the first access
+        token which can then be used to work with goodreads API.
+    """
+    CONSUMER_KEY = ''
+    CONSUMER_SECRET = ''
+    handler = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
